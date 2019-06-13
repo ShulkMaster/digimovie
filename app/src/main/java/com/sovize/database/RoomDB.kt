67ca766265921a79aa4f8d7e7f6a3eb9fa92262d
luist23 <00127717@uca.edu.sv>
2019-06-12 21:22:23 -0600

    update packaged

--- app/src/main/java/com/sovize/database/RoomDB.kt
+++ app/src/main/java/com/sovize/database/RoomDB.kt
@@ -1,2 +1,33 @@
 package com.sovize.database
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.sovize.database.daos.MovieDao
+import com.sovize.database.entities.Movie
+
+@Database(entities = [Movie::class], version = 2, exportSchema = false)
+abstract class RoomDB : RoomDatabase(){
+
+    abstract fun movieDao(): MovieDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: RoomDB? = null
+
+        fun getDatabase(appContext: Context): RoomDB {
+            if (INSTANCE == null) {
+                synchronized(RoomDB::class) {
+                    INSTANCE = Room
+                        .databaseBuilder(appContext.applicationContext
+                            , RoomDB::class.java
+                            ,"movies_db")
+                        .fallbackToDestructiveMigration()
+                        .build()
+                }
+            }
+            return INSTANCE!!
+        }
+    }
+}
--- app/src/main/java/com/sovize/database/daos/MovieDao.kt
+++ app/src/main/java/com/sovize/database/daos/MovieDao.kt
@@ -1,2 +1,21 @@
 package com.sovize.database.daos
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.sovize.database.entities.Movie
+
+@Dao
+interface MovieDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMovie(movie: Movie)
+
+    @Query("select * from Movie")
+    fun loadAllMovies(): LiveData<List<Movie>>
+
+    @Query("select * from Movie where Title like :name")
+    fun searchMovieByName(name: String): LiveData<List<Movie>>
+}
