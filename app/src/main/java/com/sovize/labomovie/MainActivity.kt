e832b2f6bb7b83db8e2dee4ba715ff303d27bcca
luist23 <00127717@uca.edu.sv>
2019-06-13 00:31:39 -0600

    preprando merge

--- app/src/main/java/com/sovize/labomovie/MainActivity.kt
+++ app/src/main/java/com/sovize/labomovie/MainActivity.kt
@@ -2,45 +2,35 @@ package com.sovize.labomovie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-<<<<<<< HEAD
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sovize.labomovie.adapters.MovieAdapter
-=======
 import android.util.Log
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.sovize.labomovie.database.entities.Movie
->>>>>>> 9fcc7dfa894b4791da3e39e90ff3d8891030a5ec
 import com.sovize.labomovie.viewmodels.MovieViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var vm: MovieViewModel
-<<<<<<< HEAD
-    private lateinit var adapterM : MovieAdapter
-=======
+    //private lateinit var adapterM : MovieAdapter
     private val observer = Observer<List<Movie>>{
         it.forEach { movie ->
             Log.d("MainAcitivty", movie.toString())
         }
     }
->>>>>>> 9fcc7dfa894b4791da3e39e90ff3d8891030a5ec
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         vm = ViewModelProviders.of(this).get(MovieViewModel::class.java)
-<<<<<<< HEAD
         vm.fetchMovie("clannad")
 
         //bind()
-=======
         vm.fetchMovie("romance")
         vm.movieList.observe(this, observer)
->>>>>>> 9fcc7dfa894b4791da3e39e90ff3d8891030a5ec
     }
 
     /*fun bind(){
