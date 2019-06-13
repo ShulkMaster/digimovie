17698e0e5148ea9b9255635a7ded3f7320c5b4cc
luist23 <00127717@uca.edu.sv>
2019-06-13 00:00:50 -0600

    ni idea2

--- app/src/main/java/com/sovize/labomovie/MainActivity.kt
+++ app/src/main/java/com/sovize/labomovie/MainActivity.kt
@@ -2,17 +2,37 @@ package com.sovize.labomovie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.sovize.labomovie.adapters.MovieAdapter
 import com.sovize.labomovie.viewmodels.MovieViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var vm: MovieViewModel
+    private lateinit var adapterM : MovieAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         vm = ViewModelProviders.of(this).get(MovieViewModel::class.java)
         vm.fetchMovie("clannad")
+
+        //bind()
     }
+
+    /*fun bind(){
+        vm = ViewModelProviders.of(this).get(MovieViewModel::class.java)
+
+        val linearLayoutManager = LinearLayoutManager(this)
+        vm.getMovieListVM().observe(this, Observer { result ->
+            adapterM.changeDataSet(result)
+        })
+
+        RecyclerView.apply {
+            adapter=adapterM
+        }
+    }*/
 }
