d88a869b8d173342749031357e23c6c3b811789d
shulkmaster <00088116@uca.edu.sv>
2019-06-12 23:52:43 -0600

    usando retrofit y corrutinas

--- app/src/main/java/com/sovize/labomovie/MainActivity.kt
+++ app/src/main/java/com/sovize/labomovie/MainActivity.kt
@@ -2,12 +2,20 @@ package com.sovize.labomovie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.sovize.labomovie.database.entities.Movie
 import com.sovize.labomovie.viewmodels.MovieViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var vm: MovieViewModel
+    private val observer = Observer<List<Movie>>{
+        it.forEach { movie ->
+            Log.d("MainAcitivty", movie.toString())
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/sovize/labomovie/client/drivers/MovieData.kt
+++ app/src/main/java/com/sovize/labomovie/client/drivers/MovieData.kt
@@ -1,13 +1,14 @@
 package com.sovize.labomovie.client.drivers
 
 import com.sovize.labomovie.client.models.ServerResponse
-import retrofit2.Call
+import kotlinx.coroutines.Deferred
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface MovieData {
 
     @GET("/")
-    fun searchTitle(@Query("s") title: String): Call<ServerResponse>
+    fun searchTitleAsync(@Query("s") title: String): Deferred<Response<ServerResponse>>
 
 }
