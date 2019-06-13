24da0b9d7b001dd2c3155fb0df4a0a458e33991e
luist23 <00127717@uca.edu.sv>
2019-06-13 00:53:45 -0600

    desmadre con merge

--- app/src/main/java/com/sovize/labomovie/MainActivity.kt
+++ app/src/main/java/com/sovize/labomovie/MainActivity.kt
@@ -2,23 +2,15 @@ package com.sovize.labomovie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-<<<<<<< HEAD
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sovize.labomovie.adapters.MovieAdapter
 import android.util.Log
-=======
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.widget.EditText
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.sovize.labomovie.adapters.MovieAdapter
->>>>>>> 6a630f0162f47e2082c0005ec348b4cd56fb09b3
 import com.sovize.labomovie.database.entities.Movie
 import com.sovize.labomovie.utils.AppLogger
 import com.sovize.labomovie.viewmodels.MovieViewModel
@@ -27,16 +19,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var vm: MovieViewModel
-<<<<<<< HEAD
-    //private lateinit var adapterM : MovieAdapter
-    private val observer = Observer<List<Movie>>{
-=======
     private lateinit var rv: RecyclerView
     private val observer = Observer<List<Movie>> {
->>>>>>> 6a630f0162f47e2082c0005ec348b4cd56fb09b3
-        it.forEach { movie ->
-            Log.d("MainAcitivty", movie.toString())
-        }
+            Log.d("MainAcitivty", it.toString())
+
         updateRecycler(it)
     }
 
@@ -45,19 +31,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         vm = ViewModelProviders.of(this).get(MovieViewModel::class.java)
-<<<<<<< HEAD
-        vm.fetchMovie("clannad")
-
         //bind()
         vm.fetchMovie("romance")
-=======
+
         rv = findViewById(R.id.RecyclerView)
->>>>>>> 6a630f0162f47e2082c0005ec348b4cd56fb09b3
+
         vm.movieList.observe(this, observer)
         setEditableRT()
     }
 
-<<<<<<< HEAD
     /*fun bind(){
         vm = ViewModelProviders.of(this).get(MovieViewModel::class.java)
 
@@ -70,7 +52,6 @@ class MainActivity : AppCompatActivity() {
             adapter=adapterM
         }
     }*/
-=======
     private fun setEditableRT() {
         findViewById<EditText>(R.id.idBuscar).addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
@@ -106,5 +87,4 @@ class MainActivity : AppCompatActivity() {
 
     }
 
->>>>>>> 6a630f0162f47e2082c0005ec348b4cd56fb09b3
 }
